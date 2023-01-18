02d8db81109cb07c57158964a48d3ea9f6b623b3
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2023-01-17 22:03:21 -0300

    add: home screen visuals

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,16 @@
-import { Habit } from "./components/Habit"
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
+// import { Habit } from "./components/Habit"
+export function App() {
 
   return (
-    <div className="bg-zinc-900 text-zinc-500">
-      <Habit completed={1} />
-      <Habit completed={2} />
-      <Habit completed={1} />
-      <Habit completed={4} />
-      <Habit completed={3} />
-      <Habit completed={1} />
-      <Habit completed={1} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header />
+          <SummaryTable />
+      </div>
     </div>
   )
 }
 
-export default App
--- web/src/components/Habit.tsx
+++
@@ -1,6 +0,0 @@
-interface HabitProps {
-    completed: number
-}
-export function Habit(props:HabitProps){
-    return <div>{props.completed}</div>
-}
