5e7d870ed4b5aa9bf4910159a6c1a833a05961bd
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2023-01-17 22:04:01 -0300

    add: routes o create, list habits and list complete habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,12 @@
 import fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./lib/routes";
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Hábito'
-            }
-        }
-    })
-    return habits
-})
 
 app.listen({
     port: 3333
