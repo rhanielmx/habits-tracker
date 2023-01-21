4515e9487d7c56e6549d105d935ded2a0e67cef1
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2023-01-21 01:38:22 -0300

    add: Finished interface and api integration

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,6 +11,15 @@ module.exports = {
       },
       gridTemplateRows: {
         7: 'repeat(7, minmax(0,1fr))'
+      },
+      keyframes: {
+        slideDown: {
+          '0%': {transform: 'translateY(-100%)'},
+          '100%': {transform: 'translateY(0%)'},
+        } 
+      },
+      animation: {
+        slideDown: 'slideDown 0.15s linear forwards'
       }
     },
   },
